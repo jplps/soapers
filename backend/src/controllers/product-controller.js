39151129ddcdd6c3ef4ac35ad807262f9f83531e
jplps <joao.pedro.pacheco@hotmail.com>
2020-05-08 17:54:19 -0300

    Implemented a few routes with no success

--- backend/src/controllers/product-controller.js
+++ backend/src/controllers/product-controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const authMiddleware = require('../middlewares/auth');
+//const authMiddleware = require('../middlewares/auth');
 const router = express.Router();
 
 const models = require('../models');
@@ -19,11 +19,10 @@ router.post('/create', async (req, res) => {
 	}
 });
 
-/*
-// Reading a specific product
+// Reading all product
 router.get('/read', async (req, res) => {
 	try {
-		const products = await models.product.findAll();
+		const products = await models.Product.findAll();
 
 		return res.status(200).send({ products });
 	} catch (err) {
@@ -32,29 +31,31 @@ router.get('/read', async (req, res) => {
 	}
 });
 
+/*
 // Updating a specific product
 router.put('/update', async (req, res) => {
-	try {
-		const products = await models.product.findAll();
-
-		return res.status(200).send({ products });
-	} catch (err) {
-		console.log(err);
-		return res.status(400).send({ err });
-	}
-});
-
-// Deleting a specific product
-router.delete('/delete', async (req, res) => {
-	try {
-		const products = await models.product.findAll();
-
-		return res.status(200).send({ products });
-	} catch (err) {
-		console.log(err);
-		return res.status(400).send({ err });
-	}
+	
 });
 */
 
-module.exports = app => app.use('/products', authMiddleware, router);
+// Deleting a specific product
+router.delete('/delete', async (req, res) => {
+	// Get the id of the product
+	const { id } = req.body;
+
+	try {
+		// Finding the product by id
+		const product = await models.Product.findOne({
+			where: { id }
+		});
+
+		// Delete it!
+		await product.destroy();
+
+		return res.status(200).send('Produto deletado.');
+	} catch (err) {
+		return res.status(400).send({ err });
+	}
+});
+
+module.exports = app => app.use('/products', router);
